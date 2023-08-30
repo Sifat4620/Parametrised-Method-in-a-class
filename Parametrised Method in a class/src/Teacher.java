810213d37615db67eee94a851e0b7cf3f3a3fe9c
Fahim Faisal Sifat <81096504+Sifat4620@users.noreply.github.com>
2023-08-30 21:19:03 +0600

    Add files via upload

---
+++ Parametrised Method in a class/src/Teacher.java
@@ -0,0 +1,21 @@
+public class Teacher {
+
+    String name ;
+
+    // set value
+
+    void setvalue(String nm){
+
+          name= nm;
+
+    }
+
+    // return
+    void displayInfo(){
+
+        System.out.println("Name:"+name);
+
+    }
+
+
+}
---
+++ Parametrised Method in a class/src/TeacherInfo.java
@@ -0,0 +1,9 @@
+public class TeacherInfo {
+    public static void main(String[] args) {
+        Teacher teacher1 = new Teacher();
+        teacher1.setvalue("Fahim");
+        teacher1.displayInfo();
+
+
+    }
+}
